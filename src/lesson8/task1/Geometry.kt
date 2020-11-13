d6f7a6279f3e45ba5e0d80c17e1fd7bb57582896
Mika <ilravgaz03@mail.ru>
2020-11-13 13:10:34 +0300

    Lessons

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -217,9 +217,7 @@ fun lineByPoints(a: Point, b: Point): Line = Line(a, abs(atan((a.y - b.y) / (a.x
  * Построить серединный перпендикуляр по отрезку или по двум точкам
  */
 fun bisectorByPoints(a: Point, b: Point): Line {
-    val halfX = (a.x + b.x) / 2.0
-    val halfY = (a.y + b.y) / 2.0
-    val line = 0
+
     return Line(a, 0.0)
 }
 
